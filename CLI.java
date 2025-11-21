609205ec1b09a1370a19033698cf9476f62c58fe
Nirnita Banik <s2412076108@ru.ac.bd>
2025-11-21 23:56:29 +0600

    added

---
+++ CLI.java
@@ -0,0 +1,11 @@
+class CLI{
+	public static void main(String args[]){
+	  int l = args.length;
+	  System.out.println("Number of arguments: " + l);
+	  for(int i=0; i<l ; i++){
+	    System.out.println("Arguements[" + i + " ]: " + args[i] );
+	  }
+  }
+}
+
+// have to put string while compiling 
